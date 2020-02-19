87fd5ca51ecd2646cd1d6059b8386c03f67cb721
Constantine.P <pkv989@gmail.com>
2020-02-20 00:04:58 +0600

    refactor demo-page & slider styles

--- src/demo-page/index.ts
+++ src/demo-page/index.ts
@@ -2,11 +2,11 @@ import './index.styl';
 import '../app/SliderJQ';
 
 $('#slider-1').rangeSlider({
-  min: -10,
-  max: 10,
-  scaleMin: -20,
-  scaleMax: 20,
-  scaleSteps: '5',
+  min: 40,
+  max: 80,
+  scaleMin: 0,
+  scaleMax: 100,
+  scaleSteps: '20',
   areTooltipsVisible: true,
   isRange: true,
   isScaleVisible: true,
@@ -16,7 +16,6 @@ $('#slider-1').rangeSlider({
 [$('#panel-11'), $('#panel-12')]);
 
 $('#slider-2').rangeSlider({
-  min: -10,
   max: 10,
   scaleMin: -20,
   scaleMax: 20,
@@ -28,3 +27,30 @@ $('#slider-2').rangeSlider({
   type: 'vertical',
 },
 [$('#panel-21'), $('#panel-22')]);
+
+$('#slider-3').rangeSlider({
+  min: -10,
+  max: 10,
+  scaleMin: -15,
+  scaleMax: 20,
+  scaleSteps: '3*5 2*10',
+  areTooltipsVisible: true,
+  isRange: true,
+  isScaleVisible: true,
+  isReverseDirection: false,
+  type: 'vertical',
+},
+[$('#panel-31'), $('#panel-32')]);
+
+$('#slider-4').rangeSlider({
+  max: 21,
+  scaleMin: -100,
+  scaleMax: 100,
+  scaleSteps: '',
+  areTooltipsVisible: true,
+  isRange: false,
+  isScaleVisible: false,
+  isReverseDirection: true,
+  type: 'horizontal',
+},
+[$('#panel-41'), $('#panel-42')]);
