1236365d1af846c9acfa5a4769309721e6a8d6dd
Constantine.P <pkv989@gmail.com>
2020-03-04 19:00:45 +0600

    remake webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 /* eslint-disable */
 const path = require('path');
+const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
-const merge = require('webpack-merge');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const common = {
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    demo: path.join(__dirname, 'src/demo-page/index.ts'),
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
@@ -37,98 +44,25 @@ const common = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name]-styles.css',
-      chunkFilename: '[id].css',
+      filename: 'style.css',
     }),
     new HTMLPlugin({
       filename: 'index.html',
       chunks: ['demo'],
-      template: path.join(__dirname, 'src', 'demo-page', 'index.html'),
+      template: path.join(__dirname, 'src/demo-page/index.html'),
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
-    new FaviconsWebpackPlugin({
-      logo: path.join(__dirname, 'src/app/styles/logo.png'),
-      mode: 'webapp',
-      devMode: 'webapp',
-      favicons: {
-        appName: 'slider-demo-page',
-        appDescription: 'slider demo page',
-        developerName: 'Constantin P.',
-        developerURL: null,
-        background: '#ddd',
-        theme_color: '#333',
-        icons: {
-          coast: false,
-          yandex: false
-        }
-      }
-    })
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'src/app/resources/favicons/**/*.*'),
+        to: path.join(__dirname, 'dist/assets/favicons/'),
+        flatten: true,
+      },
+    ]),
   ],
-};
-
-const development = {
-  devtool: 'eval',
-  entry: {
-    demo: path.join(__dirname, 'src', 'demo-page', 'index.ts'),
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-  }
-};
-
-const production = {
-  entry: {
-    demo: path.join(__dirname, 'src', 'demo-page', 'index.ts'),
-    app: path.join(__dirname, 'src', 'app', 'Slider.ts'),
-    appJQ: path.join(__dirname, 'src', 'app', 'SliderJQ.ts'),
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-  },
-  plugins: [
-    new FileManagerPlugin({
-      onEnd: {
-        mkdir: [
-          'dist/app/',
-          'dist/appJQ/',
-          'dist/demo/',
-        ],
-        copy: [
-          {source: 'dist/assets/', destination: 'dist/app/assets/'},
-          {source: 'dist/assets/', destination: 'dist/appJQ/assets/'},
-        ],
-        move: [
-          {source: 'dist/demo.js', destination: 'dist/demo/demo.js'},
-          {source: 'dist/demo.css', destination: 'dist/demo/demo.css'},
-          {source: 'dist/index.html', destination: 'dist/demo/index.html'},
-          {source: 'dist/assets/', destination: 'dist/demo/assets/'},
-          {source: 'dist/app.js', destination: 'dist/app/app.js'},
-          {source: 'dist/app.css', destination: 'dist/app/app.css'},
-          {source: 'dist/appJQ.js', destination: 'dist/appJQ/appJQ.js'},
-          {source: 'dist/appJQ.css', destination: 'dist/appJQ/appJQ.css'},
-        ]
-      }
-    })
-  ],
-};
-
-module.exports = function (env) {
-  if (env === 'production') {
-    return merge([
-      common,
-      production
-    ]);
-  }
-  if (env === 'development') {
-    return merge([
-      common,
-      development
-    ])
-  }
 };
